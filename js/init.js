9154556900ce5feec3557cb26e4375f63f4fb4e1
Emilien Casano <emilien.casano@univ-lorraine.fr>
2018-10-04 20:49:52 +0200

    new display type

--- js/init.js
+++ js/init.js
@@ -1,19 +1,117 @@
 $(document).ready(function() {
     // TODO: generer persos
-    initPerso();
+    /* TODO Creer commentaires */
+
+    //    test1();
+    initRows();
 });
 
-// var persos = require('json/persos.json');
+function readFile(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('.upload-demo').addClass('ready');
+            $uploadCrop.croppie('bind', {
+                url: e.target.result
+            }).then(function() {
+                /*  console.log('jQuery bind complete');*/
+            });
+
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    } else {
+        swal("Sorry - you're browser doesn't support the FileReader API");
+    }
+}
 
 function initPerso() {
     $.each(roaster, function(id, perso) {
-        $("#roaster").append(generateDivPerso(perso));
+        $("#roster").append(generateDivPerso(perso));
     });
 }
 
-function generateDivPerso(perso) {
+/*function generateDivPerso(perso) {
     var divPerso = '<div class="perso">';
     divPerso += '<img src="img/perso/' + perso.name.replace(/\s+/g, '_').toLowerCase() + '.png' + '" alt="' + perso.name + '">';
     divPerso += '<p class="name">' + perso.name + '</p>';
     return divPerso + '</div>'
+}*/
+function generateDivPerso(perso) {
+    var divPerso = '<div class="perso">';
+    divPerso += '<img class="image-perso" src="img/perso/' + perso.replace(/\s+/g, '_').toLowerCase() + '.png' + '" alt="' + perso + '">';
+    divPerso += '<p class="name">' + perso + '</p>';
+    return divPerso + '</div>'
+}
+
+function test1() {
+    initPerso();
+
+    $uploadCrop = $('#crop-perso').croppie({
+        enableExif: true,
+        viewport: {
+            width: 196,
+            height: 106
+        },
+        boundary: {
+            width: 300,
+            height: 300
+        }
+
+    });
+
+    $('#valide_perso').hide();
+
+    $('#upload').on('change', function() {
+        readFile(this);
+    });
+
+    $('#upload_result').on('click', function(ev) {
+        $uploadCrop.croppie('result', {
+            type: 'canvas',
+            size: 'viewport'
+        }).then(function(resp) {
+            var nom = $('#perso_name').val();
+            var img = $('<img id="tmp_upload">');
+
+            img.attr('src', resp);
+
+            $('#tmp_upload').replaceWith(img);
+
+            $('#tmp_name').text(nom);
+
+            $('#valide_perso').show();
+        });
+    });
+
+    $('#valide_perso').on('click', function(ev) {
+        var nom = $('#perso_name').val();
+        var newP = $('<p class="name">');
+        newP.text(nom);
+
+        var image = $('#tmp_upload').clone();
+
+        console.log('VALIDE ');
+
+        var newDiv = $('<div class="perso">');
+        newDiv.append(image);
+        newDiv.append(newP);
+
+        $('#roaster').append(newDiv);
+
+        $('#tmp_name').text('');
+        $('#tmp_upload').removeAttr('src');
+        $('#valide_perso').hide();
+    });
+}
+
+function initRows() {
+    $.each(rows, function(i, row) {
+        var divRow = $('<div id="row' + i + '" class="row">')
+        $.each(row, function(j, perso) {
+            divRow.append(generateDivPerso(perso));
+        });
+        $('#roster').append(divRow);
+    });
 }
